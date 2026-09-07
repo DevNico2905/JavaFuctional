e5b9c61ca9af51eefa4ceda80cb02399fc85d413
DevNico2905 <nbernal36@unisalle.edu.co>
2026-09-06 19:12:40 -0500

    Working with functions

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -7,9 +7,17 @@ public class _Function {
     static void main(String[] args) {
         int increment = increment(1);
         System.out.println(increment);
+
+        Integer increment2 = incrementByOneFn.apply(1);
+        System.out.println(increment2);
+
+        Integer multiply = multiplyBy10.apply(increment2);
+        System.out.println(multiply);
     }
 
-    Function<Integer, Integer> incrementByOneFn = number -> number++;
+    static Function<Integer, Integer> incrementByOneFn = number -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10 = number -> number * 10;
 
     static int increment(int number){
         return number + 1;
